51e34115da36fd03b45aa6dfac2b752deeea293c
Maxim Skorynin <skoryninmaksim1@gmail.com>
2019-03-04 21:25:54 +0500

    Initial commit

---
+++ tochka-test/Controllers/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tochka-test
+//
+//  Created by Maxim Skorynin on 04/03/2019.
+//  Copyright © 2019 Maxim Skorynin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
